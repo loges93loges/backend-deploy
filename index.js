e35c51c533bddeae19953274c92ea8d74f352032
Loges k <loges93321@gmail.com>
2024-07-22 20:40:14 +0530

    loges

---
+++ index.js
@@ -0,0 +1,21 @@
+function generateCertificate() {
+    const name = prompt('Enter the name:');
+    const course = prompt('Enter the course:');
+    const date = prompt('Enter the date:');
+    const standard = prompt('mention the:');
+
+
+    document.getElementById('cert-name').textContent = name;
+    document.getElementById('cert-course').textContent = course;
+    document.getElementById('cert-date').textContent = date;
+    document.getElementById('standard').textContent = standard;
+
+    html2canvas(document.getElementById('certificate'), {
+        onrendered: function(canvas) {
+            const imgData = canvas.toDataURL('image/png');
+            const pdf = new jsPDF();
+            pdf.addImage(imgData, 'PNG', 0, 0);
+            pdf.save('certificate.pdf');
+        }
+    });
+}
